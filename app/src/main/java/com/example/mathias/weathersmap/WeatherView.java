3888c97e8159d7d92a6f450c542732d15e4b18e6
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2016-09-26 16:42:04 +0200

    empty project

---
+++ app/src/main/java/com/example/mathias/weathersmap/WeatherView.java
@@ -0,0 +1,13 @@
+package com.example.mathias.weathersmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_view);
+    }
+}
